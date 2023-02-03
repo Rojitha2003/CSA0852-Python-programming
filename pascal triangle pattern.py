3c99d947132af84cc1f1bd884dbc06d155b16c8d
Rojitha2003 <110800534+Rojitha2003@users.noreply.github.com>
2023-02-03 13:58:56 +0530

    Add files via upload

---
+++ pascal triangle pattern.py
@@ -0,0 +1,12 @@
+rows=int(input("Enter number of rows:"))
+number=1
+for i in range(1,rows+1):
+    for j in range(1,rows-i+1):
+        print(" ",end="")
+    for k in range(0,i):
+        if k==0 or i==0:
+            number=1
+        else:
+            number=number*(i-k)//k
+        print(number,end=" ")
+    print()
