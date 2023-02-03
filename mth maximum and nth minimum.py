f6c3d35aa7459304da2eaa54a9ea6889b57a76cc
Rojitha2003 <110800534+Rojitha2003@users.noreply.github.com>
2023-02-03 10:05:52 +0530

    day 2

---
+++ mth maximum and nth minimum.py
@@ -0,0 +1,18 @@
+a=[]
+j=int(input("Enter the number of elements:"))
+print("Enter the elements of the list:")
+for i in range(0,j):
+    x=int(input())
+    a.append(x)
+a.sort()
+m=int(input("Enter m value:"))
+n=int(input("Enter n value:"))
+if(m<0 or n<0 or m>j or n>j):
+    print("Invalid input")
+else:
+    sum=a[j-m]+a[n-1]
+    sub=a[j-m]-a[n-1]
+    print("Mth maximum element=",a[j-m])
+    print("Nth minimum element=",a[n-1])
+    print("Sum=",sum)
+    print("Difference=",sub)
