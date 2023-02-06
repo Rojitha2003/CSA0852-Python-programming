a7cd29c99874346f0952ee524043cd02d2f4ca75
Rojitha2003 <110800534+Rojitha2003@users.noreply.github.com>
2023-02-06 09:52:05 +0530

    day 3

---
+++ sum of square of even and odd numbers.py
@@ -0,0 +1,14 @@
+a=[]
+even=0
+odd=0
+n=int(input("Enter the number of elements in list:"))
+for i in range(0,n):
+    x=int(input("Enter the elements:"))
+    a.append(x)
+for j in a:
+    if j%2==0:
+        even=even+j**2
+    else:
+        odd=odd+j**2
+print('[',odd,',',even,']')
+        
